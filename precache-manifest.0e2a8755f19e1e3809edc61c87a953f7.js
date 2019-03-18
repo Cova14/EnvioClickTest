29df44ccbbf490143fc00797198848916fbdabaf
Cova14 <arty1498@hotmail.com>
2019-03-18 15:25:34 -0600

    Updates

---
+++ precache-manifest.0e2a8755f19e1e3809edc61c87a953f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84b133ecd03b8c40b276",
+    "url": "/EnvioClickTest/static/js/runtime~main.3a501889.js"
+  },
+  {
+    "revision": "6279e6d25a14f16e910e",
+    "url": "/EnvioClickTest/static/js/main.14707baf.chunk.js"
+  },
+  {
+    "revision": "1c5bf73e23c114c6a136",
+    "url": "/EnvioClickTest/static/js/2.f01e8342.chunk.js"
+  },
+  {
+    "revision": "6279e6d25a14f16e910e",
+    "url": "/EnvioClickTest/static/css/main.a8d59c4c.chunk.css"
+  },
+  {
+    "revision": "7605732d21f4a6579897ab73ff97dd42",
+    "url": "/EnvioClickTest/index.html"
+  }
+];
